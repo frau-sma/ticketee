c00491744b65494b81d5a09bb6decc77818f5dad
Frau Sma <frau.sma@gmail.com>
2011-11-11 00:56:02 +0100

    The user is now redirected if he tries to access a non-existent project.

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe ProjectsController do
-
+  it 'displays an error for a missing project' do
+    get :show, :id => 'not-here'
+    response.should redirect_to(projects_path)
+    flash[:alert].should == 'The project you were looking for does not exist.'
+  end
 end
