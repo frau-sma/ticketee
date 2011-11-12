71f238c80b57e8ab2a98f3915cd8b61c9f14ddeb
Frau Sma <frau.sma@gmail.com>
2011-11-12 15:42:41 +0100

    Returning users may now sign in using a sign-in form.

--- features/signing_in.feature
+++ features/signing_in.feature
@@ -4,8 +4,15 @@ Feature: Signing in
   I want to be able to sign in
 
   Scenario: Signing in via confirmation
-    Given there is a user
-    And "user@ticketee.com" opens the email with subject "Confirmation instructions"
+    Given I am an unconfirmed user
+    And "user@ticketee.org" opens the email with subject "Confirmation instructions"
     And they click the first link in the email
     Then I should see the registration confirmation message
     And I should be signed in
+
+  Scenario: Signing in via form
+    Given I am a confirmed user
+    And I am on the home page
+    When I navigate to the sign-in page
+    And I fill in my user details
+    Then I should be signed in
--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -13,8 +13,8 @@ Then /^I should see the sign\-up verification message$/ do
   page.should have_content 'You have signed up successfully.'
 end
 
-Given /^there is a user$/ do
-  @user = User.create!(:email => 'user@ticketee.com', :password => 'password')
+Given /^I am an unconfirmed user$/ do
+  @user = User.create!(:email => 'user@ticketee.org', :password => 'password')
 end
 
 Then /^I should see the registration confirmation message$/ do
@@ -22,5 +22,20 @@ Then /^I should see the registration confirmation message$/ do
 end
 
 Then /^I should be signed in$/ do
-  page.should have_content 'Signed in [user@ticketee.com]'
+  page.should have_content 'Signed in [user@ticketee.org]'
+end
+
+Given /^I am a confirmed user$/ do
+  @user = User.create!(:email => 'user@ticketee.org', :password => 'password')
+  @user.confirm!
+end
+
+When /^I navigate to the sign\-in page$/ do
+  click_link 'Sign In'
+end
+
+When /^I fill in my user details$/ do
+  fill_in 'Email', :with => 'user@ticketee.org'
+  fill_in 'Password', :with => 'password'
+  click_button 'Sign in'
 end
